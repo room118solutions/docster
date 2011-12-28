6108ff6d8c3c24fdd547add6ca0ad125e04968bd
Jim Ryan <jim@room118solutions.com>
2011-12-28 00:38:24 -0500

    Added support for ruby docs, changed location of merged project docs

--- lib/docster/cli.rb
+++ lib/docster/cli.rb
@@ -8,9 +8,11 @@ module Docster
     desc 'generate', "Generate merged sdoc documentation for all :default and :development gems in this project"
     method_option :groups, :default => ['default', 'development'], :type => :array, :aliases => '-g'
     method_option :name, :default => Bundler.default_gemfile.dirname.split.last.to_s, :type => :string, :aliases => '-n'
+    method_option :ruby_version, :default => "#{RUBY_VERSION}-p#{RUBY_PATCHLEVEL}", :type => :string, :aliases => '-r', :desc => 'Ruby version to include', :banner => '(VERSION)-p(PATCHLEVEL)'
+    method_option :without_ruby, :default => false, :type => :boolean, :desc => 'Generate documentation without Ruby docs'
     def generate
-      DocGenerator.generate! options.name, options.groups
+      DocGenerator.generate! options.name, options.groups, (options.without_ruby ? nil : options.ruby_version)
     end
-       
+
   end
 end
--- lib/docster/doc_generator.rb
+++ lib/docster/doc_generator.rb
@@ -2,34 +2,44 @@ require 'bundler'
 
 module Docster
   class DocGenerator
-    def self.generate!(project_name, groups)
+    def self.generate!(project_name, groups, ruby_version)
       @@groups = groups
       
-      create_gems_directory! unless Dir.exists?(File.join(user_docster_path, 'gems'))
+      create_docs_directory! unless Dir.exists?(docs_dir)
+      create_projects_directory! unless Dir.exists?(projects_path)
       
       changes = false
       gems.each do |name, info|
         unless Dir.exists?(doc_path_for name, info[:version])
-          generate_sdoc_for(name, info)
+          generate_sdoc_for :type => :gem, :name => name, :version => info[:version], :path => info[:path]
           changes = true
         end
       end
       
-      if changes || !File.exists?(File.join user_docster_path, project_name, 'index.html')
-        FileUtils.rm_rf File.join(user_docster_path, project_name)
-        sdoc_merge project_name
+      unless ruby_version.nil? || Dir.exists?(doc_path_for 'ruby', ruby_version)
+        generate_sdoc_for :name => 'ruby', :version => ruby_version, :path => download_ruby(ruby_version)
+        changes = true
       end
       
-      `open #{File.join user_docster_path, project_name, 'index.html'}`
+      if changes || !File.exists?(File.join(project_path_for(project_name), 'index.html'))
+        FileUtils.rm_rf project_path_for project_name
+        sdoc_merge project_name, ruby_version
+      end
+      
+      cleanup!
+      
+      `open #{File.join project_path_for(project_name), 'index.html'}`
     end
     
   private
-    def self.sdoc_merge(project_name)
-      `sdoc-merge --title "#{project_name}" --op "#{File.join user_docster_path, project_name}" --names "#{gem_names}" #{doc_paths}`
+    def self.sdoc_merge(project_name, ruby_version)
+      names = ruby_version ? [gem_names, 'ruby'].join(',') : gem_names
+      paths = ruby_version ? doc_paths << %Q( "#{doc_path_for 'ruby', ruby_version}") : doc_paths
+      `sdoc-merge --title "#{project_name}" --op "#{project_path_for project_name}" --names "#{names}" #{paths}`
     end
     
-    def self.generate_sdoc_for(name, info)
-      `sdoc -o "#{doc_path_for name, info[:version]}" "#{info[:path]}"`
+    def self.generate_sdoc_for(options = {})      
+      `sdoc -o "#{doc_path_for options[:name], options[:version]}" "#{options[:path]}"`
     end
     
     def self.gems
@@ -56,12 +66,46 @@ module Docster
       @@user_docster_path = File.join(Etc.getpwuid.dir, '.docster')
     end
     
-    def self.doc_path_for(gem_name, gem_version)
-      File.join(user_docster_path, 'gems', gem_name, gem_version)
+    def self.doc_path_for(name, version)
+      File.join user_docster_path, 'docs', name, version
     end
     
-    def self.create_gems_directory!
-      FileUtils.mkdir_p File.join(user_docster_path, 'gems')
+    def self.create_docs_directory!
+      FileUtils.mkdir_p docs_dir
+    end
+    
+    def self.create_projects_directory!
+      FileUtils.mkdir_p projects_path
+    end
+    
+    def self.docs_dir
+      File.join(user_docster_path, 'docs')
+    end
+    
+    def self.tmp_path
+      path = File.join(user_docster_path, 'tmp')
+      FileUtils.mkdir_p path
+      path
+    end
+    
+    def self.cleanup!
+      FileUtils.rm_rf tmp_path
+    end
+    
+    def self.projects_path
+      File.join user_docster_path, 'projects'
+    end
+    
+    def self.project_path_for(project_name)
+      File.join projects_path, project_name
+    end
+    
+    def self.download_ruby(version)
+      ruby_archive = "ruby-#{version}.tar.bz2"
+      archive_path = File.join tmp_path, ruby_archive
+      `wget http://ftp.ruby-lang.org/pub/ruby/#{version.split('.')[0..1].join('.')}/#{ruby_archive} -O "#{archive_path}"`
+      `tar -xf "#{archive_path}" -C "#{tmp_path}"`
+      File.join tmp_path, "ruby-#{version}"
     end
   end
 end
